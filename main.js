32f082ce7fd972b5ffa78e56a59a7c29bcd6118d
BruceChen034020 <2017greatplatform@gmail.com>
2017-07-10 18:09:16 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,24 +2,20 @@ var button1, button2;
 var slider1;
 var img1, img2;
 
-function preload(){
-  img1 = loadImage("Project003/001.jpg");
-  img2 = loadImage("Project003/002.jpg");
-}
-
 function setup(){
   createCanvas(800, 600);
   background(0);
-  button1 = createButton("鍾凱恩");
-  button2 = createButton("張頌宇");
+  button1 = createButton("黃于軒");
+  button2 = createButton("楊鈞任");
   createP('');
   slider1 = createSlider();
-  img1 = loadImage("001.jpg");
-  img2 = loadImage("002.jpg");
+  img1 = loadImage("Huang.jpg");
+  img2 = loadImage("Yang.jpg");
   button1.mousePressed(DisplayImg1);
   image(img1, 0, 0);
   image(img1, 0, 0, 100, 100);
   image(img1, 0, 0, 100, 100, 0, 0, 100, 100);
+  console.log(img1.toString());
 }
 
 function draw(){
